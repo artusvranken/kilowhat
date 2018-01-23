c9c5e894bcbe161338bd7d12beda8ff55c8bba18
artusvranken <artusvranken@gmail.com>
2018-01-23 01:16:42 +0100

    Tests for MeterRepositoryStub complete.

--- test/MeterRepositoryStubTests.ts
+++ test/MeterRepositoryStubTests.ts
@@ -1,6 +1,8 @@
 import { MeterRepositoryStub } from '../src/MeterRepositoryStub';
 import { Meter } from '../src/Meter';
 import { MeterEvent } from '../src/MeterEvent';
+import { Dial } from '../src/Dial';
+import { Reading } from '../src/Reading';
 import * as assert from 'assert';
 
 describe('MeterRepositoryStub', () => {
@@ -126,6 +128,55 @@ describe('MeterRepositoryStub', () => {
             assert.equal(newStub.getMeterEvents(newMeter.id).size, 1);
            
             assert.equal(newStub.removeMeterEvent(newMeter.id, newMeterEvent.id), 0);
+        });
+    });
+    
+    describe('addDial', () => {
+        it('should add a Dial and return it.', () => {
+            let newStub = new MeterRepositoryStub();
+            let newMeter = newStub.addMeter(new Meter());
+            let newDial = new Dial();
+            let returnedDial = newStub.addDial(newMeter.id, newDial);
+            
+            assert.equal(newDial, returnedDial);
+        });
+    });
+    
+    describe('removeDial', () => {
+        it('should remove a Dial from a Meter and return the amount of Dials maintained by the Meter.', () => {
+            let newStub = new MeterRepositoryStub();
+            let newMeter = newStub.addMeter(new Meter());
+            let newDial = newStub.addDial(newMeter.id, new Dial());
+            
+            assert.equal(newStub.getDials(newMeter.id).size, 1);
+            
+            assert.equal(newStub.removeDial(newMeter.id, newDial.id), 0);
+        });
+    });
+    
+    describe('addReading', () => {
+        it('should add a new Reading to a Dial and return it.', () => {
+            let newStub = new MeterRepositoryStub();
+            let newMeter = newStub.addMeter(new Meter());
+            let newDial = newStub.addDial(newMeter.id, new Dial());
+            let newReading = new Reading();
+            let returnedReading = newStub.addReading(newMeter.id, newDial.id, newReading);
+            
+            assert.equal(newReading, returnedReading);
+        });
+    });
+    
+    describe('removeReading', () => {
+        it('should remove a Reading and return the amount of Readings maintained by the Dial.', () => {
+            let newStub = new MeterRepositoryStub();
+            let newMeter = newStub.addMeter(new Meter());
+            let newDial = newStub.addDial(newMeter.id, new Dial());
+            let newReading = new Reading();
+            let returnedReading = newStub.addReading(newMeter.id, newDial.id, newReading);
+            
+            assert.equal(newReading, returnedReading);
+            
+            assert.equal(newStub.removeReading(newMeter.id, newDial.id, newReading.id), 0);
         });
     });
     
